ee0b8caa01dab73914737bce7b12a6d1c7e160b7
alexsandrov16 <alejandro920131@gmail.com>
2022-07-04 16:36:40 -0400

    Initial Commit

---
+++ mint/File/ManagerFile.php
@@ -0,0 +1,66 @@
+<?php
+defined('MINT') || die;
+
+namespace Mint\File;
+
+/**
+ * undocumented class
+ */
+class ManagerFile
+{
+    /**
+     * undocumented function summary
+     *
+     * Undocumented function long description
+     *
+     * @param Type $var Description
+     * @return type
+     * @throws conditon
+     **/
+    public function __construct()
+    {
+        
+    }
+
+    /**
+     * undocumented function summary
+     *
+     * Undocumented function long description
+     *
+     * @param Type $var Description
+     * @return type
+     * @throws conditon
+     **/
+    public function allFiles()
+    {
+        # code...
+    }
+
+    /**
+     * undocumented function summary
+     *
+     * Undocumented function long description
+     *
+     * @param Type $var Description
+     * @return type
+     * @throws conditon
+     **/
+    public function allDir()
+    {
+        # code...
+    }
+
+    /**
+     * undocumented function summary
+     *
+     * Undocumented function long description
+     *
+     * @param Type $var Description
+     * @return type
+     * @throws conditon
+     **/
+    public function allFileExt(string $ext)
+    {
+        # code...
+    }
+}
